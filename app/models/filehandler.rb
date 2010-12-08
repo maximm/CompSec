83b986e8a1fac9265f65cb2993ea47c40e5b60ea
Maxim Machalek <maximm2k@gmail.com>
2010-12-08 13:42:09 +0100

    file path deleted

--- app/models/filehandler.rb
+++ app/models/filehandler.rb
@@ -4,27 +4,30 @@ class Filehandler < ActiveRecord::Base
   def self.save(file)
     filename = file['File'].original_filename
     
-    File.open('C:/compseckeystore', "wb") { |f| f.write(file['File'].read)}
+    File.open('compseckeystore', "wb") { |f| f.write(file['File'].read)}
     return filename
   end
   
   def self.approve(password)
-    #password = "bestpassever123" 
+   
     # Check that the password is correct
-
-    if !`keytool -list -v -keystore "C:/compseckeystore" -storepass #{password} | grep -c 'kedja: 2'`["1"].nil? then
-      return "Congratulations, your certifcate has the correct length of 2"
-    elsif !`keytool -list -v -keystore "C:/compseckeystore" -storepass #{password} | grep -c 'Keystore was tampered with, or password was incorrect'`["2"].nil? then
+    if password.empty?
+      return "Password can not be empty"
+    elsif !`keytool -list -v -keystore "compseckeystore" -storepass #{password} | grep -c 'Keystore was tampered with, or password was incorrect'`["2"].nil? then
       return "The password was incorrect"
+    end
+
+    # Check if the certificate chain matches 2
+    if !`keytool -list -v -keystore "compseckeystore" -storepass #{password} | grep -c 'kedja: 2'`["1"].nil? then
+      return "Congratulations, your certifcate has the correct length of 2"
     else
       return "Sorry, your certificate chain does not contain the correct number of certificates"
     end
   end
   
   def self.cleanup
-    if File.exist?('C:\compseckeystore') then
-      File.delete('C:/compseckeystore') 
+    if File.exist?('compseckeystore') then
+      File.delete('compseckeystore') 
     end
   end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Uploader::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
-  resources :upload
+ 
+   resources :upload
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
