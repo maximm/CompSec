de380d82fc3393ced788540111ad09c93bab8689
Maxim Machalek <maximm2k@gmail.com>
2011-01-17 14:52:32 +0100

    Testing to heroku

--- crt/proj1.sh
+++ crt/proj1.sh
@@ -4,7 +4,7 @@
 openssl genrsa -out CA_key.pem 1024
 openssl genrsa -out client_key.pem 1024
 #Create CA.crt
-openssl req -new -x509 -key CA_key.pem -out CA.crt -subj /CN="Robert&Maxim"/OU="datasek"/O="LTH"/L="lund"/ST="skane"/C="se"/
+openssl req -new -x509 -key CA_key.pem -out CA.crt -subj /CN="cim04mm6"/OU="datasek"/O="LTH"/L="lund"/ST="skane"/C="se"/
 #Create a scr
 openssl req -new -key client_key.pem -out client.csr -subj /CN="Bruce Lee"/OU=seattle/O=Hollywood/L=anykey/ST=washington/C=us
 #Sign client.csr with the CA's certificate
@@ -15,7 +15,7 @@ keytool -genkeypair -dname "cn=Lady Gaga, ou=torment dept, o=succubi inc, l=9th 
 #Make a certificate signing request for Lady Gaga called gaga.csr
 keytool -certreq -file gaga.csr -alias gaga -keypass totally666 -keystore compseckeystore -storepass bestpassever123
 #Sign gaga.csr with the CA's certificate
-openssl x509 -req -CA CA.crt -CAkey CA_key.pem -in gaga.csr -out gaga.crt 
+openssl x509 -req -CA CA.crt -CAkey CA_key.pem -CAcreateserial -in gaga.csr -out gaga.crt 
 #import the CA to the keystore
 keytool -importcert -alias ca -file CA.crt -keystore compseckeystore -storepass bestpassever123 -noprompt
 #import Lady Gaga's certificate
