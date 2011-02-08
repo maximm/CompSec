c50007ef4f8ee7cbcc6d062ada0ab574cfe8ddc2
Maxim Machalek <maximm2k@gmail.com>
2011-02-08 13:01:22 +0100

    edited create page

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -19,13 +19,18 @@ class StudentController < ApplicationController
     s.age = 40 + r.rand(10)
     s.sex = "Male"
     @db << s
+
+    # Query Creator
+    @query = "Select * FROM Students WHERE"
     
     # Masters
     if !params[:s].nil? && !params[:s]['masters'].empty?
       if Integer(params[:mi]['1']) == 1 then
         @db.delete_if { |s| s.masters == params[:s]['masters'] }
+        @query += " Masters <> '#{params[:s]['masters']}' AND"
       else
         @db.delete_if { |s| s.masters != params[:s]['masters'] }
+        @query += " Masters = '#{params[:s]['masters']}' AND"
       end 
     end
     
@@ -33,17 +38,29 @@ class StudentController < ApplicationController
     if !params[:s].nil? && params[:s]['age'].is_integer? then
       if Integer(params[:ai]['1']) == 1  then
         @db.delete_if { |s| s.age == Integer(params[:s]['age']) }  
+        @query += " Age  <> '#{params[:s]['age']}' AND"
       else
         @db.delete_if { |s| s.age != Integer(params[:s]['age']) }
+        @query += " Age  = '#{params[:s]['age']}' AND"
       end
     end
     
     # Sex
     if !params[:s].nil? && params[:gender] == "male" then
       @db.delete_if { |s| s.sex == "Female" }
+      @query += " Sex = Female"
     elsif !params[:s].nil? && params[:gender] == "female" then
       @db.delete_if { |s| s.sex == "Male" }
+      @query += " Sex = Male"
     end
+    
+    # Query scrub
+    if @db.size == 500 then
+      @query = "Select * FROM Students"
+    elsif @query.end_with?(" AND") then
+      @query =  @query[0..@query.size-4]
+    end
+    
     
     if @db.size > 0 then
       @mean = @db.sum { |s| s.score } / Float(@db.size)
@@ -60,7 +77,8 @@ class StudentController < ApplicationController
       else
         @notice = "<font color='red' size='5'>Answer not correct. Please try again</font>"
       end
-    end       
+    end
+ 
   end
 end
 
