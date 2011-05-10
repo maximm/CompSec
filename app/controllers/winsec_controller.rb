c69b7f3d418695d37f86997e865f11d32049f8e2
Maxim Machalek <maximm2k@gmail.com>
2011-05-10 16:51:51 +0200

    updated

--- app/controllers/winsec_controller.rb
+++ app/controllers/winsec_controller.rb
@@ -6,54 +6,86 @@ class WinsecController < ApplicationController
   def create
     @username = params[:un]['un']
     @files = Array.new
-    rand = Random.new(@username.hash)
+    @r = Random.new(@username.hash)
+    questions = Array.new
+    
+    # Users
+    
 
-    # File 1    
-    file = Winfile.new("readme.txt")
+    # File 1 - General knowledge
+    file = Winfile.new("readme.xml")
     file.dacl << Ace.new("deny", "Alice", "Execute")
     file.dacl << Ace.new("deny", "Alice", "Write")
     file.dacl << Ace.new("deny", "Bob", "Read")
     file.dacl << Ace.new("deny", "Bob", "Full Control")
     file.dacl << Ace.new("allow", "Everyone", "Full Control")
 
-            
-    file.questions << WinQuestion.new("Will Alice be allowed to gain the following permissions: [Execute, Write]?", @r.rand.to_s, false, params)
-    file.questions << WinQuestion.new("Will Alice be allowed to gain the following permissions: [Read]?", @r.rand.to_s, true, params)
-    file.questions << WinQuestion.new("Will Eve be allowed to gain the following permissions: [Read]?", @r.rand.to_s, true, params)
-    file.questions << WinQuestion.new("Will Eve be allowed to gain the following permissions: [Read, Write]?", @r.rand.to_s, true, params)
-    file.questions << WinQuestion.new("Will Bob be allowed to gain the following permissions: [Read]?", @r.rand.to_s, false, params)
-    file.questions << WinQuestion.new("Will Bob be allowed to gain the following permissions: [Full Control]?", @r.rand.to_s, false, params)
-    file.questions << WinQuestion.new("Will Bob be allowed to gain the following permissions: [Write]?", @r.rand.to_s, false, params)
+    questions = Array.new    
+    questions << WinQuestion.new("Will Alice be allowed to gain the following permissions: [Execute, Write]?", @r.rand.to_s, false, params)
+    questions << WinQuestion.new("Will Alice be allowed to gain the following permissions: [Read]?", @r.rand.to_s, true, params)
+    questions << WinQuestion.new("Will Eve be allowed to gain the following permissions: [Read]?", @r.rand.to_s, true, params)
+    questions << WinQuestion.new("Will Eve be allowed to gain the following permissions: [Read, Write]?", @r.rand.to_s, true, params)
+    questions << WinQuestion.new("Will Bob be allowed to gain the following permissions: [Read]?", @r.rand.to_s, false, params)
+    questions << WinQuestion.new("Will Bob be allowed to gain the following permissions: [Full Control]?", @r.rand.to_s, false, params)
+    questions << WinQuestion.new("Will Bob be allowed to gain the following permissions: [Write]?", @r.rand.to_s, false, params)
+    file.randomizeQuestions(3, questions, @r)
     @files << file    
     
-    # File 2
-    file = Winfile.new("data.exe")
+    # File 2 - General Knowledge
+    file = Winfile.new("data.txt")
 
     file.dacl << Ace.new("deny", "Everyone", "Write")
-    file.dacl << Ace.new("allow", "Everyone", "Read")
     file.dacl << Ace.new("deny", "Alice", "Read")
-    file.dacl << Ace.new("allow", "Alice", "Read")
     file.dacl << Ace.new("deny", "Bob", "Read")
+    file.dacl << Ace.new("allow", "Everyone", "Read")
+    file.dacl << Ace.new("allow", "Alice", "Read")
     file.dacl << Ace.new("allow", "Bob", "Write")
     
-    file.questions << WinQuestion.new("Will Alice be allowed to gain the following permissions: [Read]?", @r.rand.to_s, true, params)
-    file.questions << WinQuestion.new("Will Alice be allowed to gain the following permissions: [Read, Write]?", @r.rand.to_s, false, params)
-    file.questions << WinQuestion.new("Will Alice be allowed to gain the following permissions: [Execute]?", @r.rand.to_s, false, params)
-    file.questions << WinQuestion.new("Will Bob be allowed to gain the following permissions: [Read, Write]?", @r.rand.to_s, false, params)
-
+    questions = Array.new
+    questions << WinQuestion.new("Will Alice be allowed to gain the following permissions: [Read]?", @r.rand.to_s, false, params)
+    questions << WinQuestion.new("Will Alice be allowed to gain the following permissions: [Read, Write]?", @r.rand.to_s, false, params)
+    questions << WinQuestion.new("Will Alice be allowed to gain the following permissions: [Execute]?", @r.rand.to_s, false, params)
+    questions << WinQuestion.new("Will Bob be allowed to gain the following permissions: [Read, Write]?", @r.rand.to_s, false, params)
+    questions << WinQuestion.new("Will Bob be allowed to gain the following permissions: [Read]?", @r.rand.to_s, true, params)
+    questions << WinQuestion.new("Will Eve be allowed to gain the following permissions: [Read]?", @r.rand.to_s, true, params)
+    questions << WinQuestion.new("Will Eve be allowed to gain the following permissions: [Read, Execute]?", @r.rand.to_s, false, params)
+    file.randomizeQuestions(3, questions, @r)
     @files << file
     
     
-    # File 3
+    # File 3 - Empty DACL
     file = Winfile.new("report.ppt")
-
-    file.questions << WinQuestion.new("Will Bob be allowed to gain the following permissions: [Execute, Write]?", @r.rand.to_s, false, params)
-    file.questions << WinQuestion.new("Will Alice be allowed to gain the following permissions: [Write]?", @r.rand.to_s, false, params)
-
+    
+    questions = Array.new
+    questions << WinQuestion.new("Will Bob be allowed to gain the following permissions: [Execute, Write]?", @r.rand.to_s, false, params)
+    questions << WinQuestion.new("Will Bob be allowed to gain the following permissions: [Read]?", @r.rand.to_s, false, params)
+    questions << WinQuestion.new("Will Alice be allowed to gain the following permissions: [Write]?", @r.rand.to_s, false, params)
+    questions << WinQuestion.new("Will Alice be allowed to gain the following permissions: [Read, Execute]?", @r.rand.to_s, false, params)
+    file.randomizeQuestions(2, questions, @r)
     @files << file    
     
+    # File 4 - ACEs containt groups mixed with users
+    file = Winfile.new("LoveVirus.exe")
+
+    file.dacl << Ace.new("deny", "Students", "Execute")
+    file.dacl << Ace.new("deny", "Alice", "Execute")
+    file.dacl << Ace.new("deny", "Bob", "Execute")
+    file.dacl << Ace.new("allow", "Teachers", "Execute")
+    file.dacl << Ace.new("allow", "Students", "Read")
+    file.dacl << Ace.new("allow", "Students", "Write")
     
-    # File 2
+    questions = Array.new
+    questions << WinQuestion.new("Will Bob (Teachers) be allowed to gain the following permissions: [Write, Execute]?", @r.rand.to_s, false, params)
+    questions << WinQuestion.new("Will Bob (Teachers) be allowed to gain the following permissions: [Read]?", @r.rand.to_s, false, params)
+    questions << WinQuestion.new("Will Eve (Teachers) be allowed to gain the following permissions: [Execute]?", @r.rand.to_s, true, params)
+    questions << WinQuestion.new("Will Alice (Students) be allowed to gain the following permissions: [Read, Execute]?", @r.rand.to_s, false, params)
+    questions << WinQuestion.new("Will Alice (Students) be allowed to gain the following permissions: [Read]?", @r.rand.to_s, true, params)
+    questions << WinQuestion.new("Will Alice (Students) be allowed to gain the following permissions: [Write]?", @r.rand.to_s, true, params)
+    file.randomizeQuestions(3, questions, @r)
+    @files << file   
+
+
+    # File 5 - Deny and Allowed mixed including groups
     file = Winfile.new("MsPaint.exe")
 
     file.dacl << Ace.new("deny", "Students", "Execute")
@@ -62,11 +94,15 @@ class WinsecController < ApplicationController
     file.dacl << Ace.new("deny", "Bob", "Execute")
     file.dacl << Ace.new("allow", "Students", "Read")
     
-    file.questions << WinQuestion.new("Will Alice (Students) be allowed to gain the following permissions: [Execute]?", @r.rand.to_s, false, params)
-    file.questions << WinQuestion.new("Will Bob (Teachers) be allowed to gain the following permissions: [Execute]?", @r.rand.to_s, true, params)
-    file.questions << WinQuestion.new("Will Bob (Teachers) be allowed to gain the following permissions: [Read, Execute]?", @r.rand.to_s, false, params)
-    file.questions << WinQuestion.new("Will Alice (Students) be allowed to gain the following permissions: [Read]?", @r.rand.to_s, true, params)
-    file.questions << WinQuestion.new("Will Alice (Students) be allowed to gain the following permissions: [Read, Execute]?", @r.rand.to_s, false, params)
+    questions = Array.new
+    questions << WinQuestion.new("Will Alice (Students) be allowed to gain the following permissions: [Execute]?", @r.rand.to_s, false, params)
+    questions << WinQuestion.new("Will Alice (Students) be allowed to gain the following permissions: [Read]?", @r.rand.to_s, true, params)
+    questions << WinQuestion.new("Will Alice (Students) be allowed to gain the following permissions: [Read, Execute]?", @r.rand.to_s, false, params)
+    questions << WinQuestion.new("Will Bob (Teachers) be allowed to gain the following permissions: [Execute]?", @r.rand.to_s, true, params)
+    questions << WinQuestion.new("Will Bob (Teachers) be allowed to gain the following permissions: [Read, Execute]?", @r.rand.to_s, false, params)
+    questions << WinQuestion.new("Will Eve (Teachers) be allowed to gain the following permissions: [Read, Execute]?", @r.rand.to_s, false, params)
+    questions << WinQuestion.new("Will Eve (Teachers) be allowed to gain the following permissions: [Execute]?", @r.rand.to_s, false, params)
+    file.randomizeQuestions(3, questions, @r)
     @files << file    
 
     @correct = 0
--- app/models/winfile.rb
+++ app/models/winfile.rb
@@ -5,8 +5,15 @@ class Winfile
     @filename = filename
     @dacl = Array.new
     @questions = Array.new
-
   end
+  
+  def randomizeQuestions(nbr, questions, rand)
+      nbr.times do
+      question = questions[rand.rand(questions.length)]
+      questions.delete_if { |q| q.name == question.name }
+      @questions << question
+    end
+  end  
 end
 
 class WinQuestion
@@ -27,4 +34,16 @@ class WinQuestion
       return params[filename][filename] == 'true'
     end
   end  
+end
+
+class WinToken
+  attr_accessor :user, :groups, :restrictedsid
+  def initialize(user, group)
+    @user = user
+    @group = group
+  end
+  
+  def to_s
+    @user
+  end
 end
